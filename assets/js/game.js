70b29341fb6a6955a854cc45ca830b994f60623e
BonnieMcb <bonniemcb@gmail.com>
2020-10-17 11:55:00 +0000

    Fixing validation errors and whitespace in js files

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,181 +1,172 @@
 $(document).ready(function() {
 
-    // NOTE: "Category" isn't being used right now. See Readme for details.
-    let difficulty = sessionStorage.getItem("difficulty");
-    // Default to medium.
-    if (difficulty == null) {
-        difficulty = "medium";
-    }
+	// NOTE: "Category" isn't being used right now. See Readme for details.
+	let difficulty = sessionStorage.getItem("difficulty");
+	// Default to medium.
+	if (difficulty === null) {
+		difficulty = "medium";
+	}
 
-    let difficulties = {
-        "medium": 16,
-        "hard": 36
-    };
+	let difficulties = {
+		"medium": 16,
+		"hard": 36
+	};
 
-    let maxTiles = difficulties[difficulty];
-    let pairNumber = maxTiles / 2;
+	let maxTiles = difficulties[difficulty];
+	let pairNumber = maxTiles / 2;
 
-    $.getJSON("assets/data/birds.json", function(json) {
+	$.getJSON("assets/data/birds.json", function(json) {
+		// Shuffle the data to get random data each time
+		shuffle(json);
 
-    // Shuffle the data to get random data each time
-    shuffle(json);
+		let gameData = [];
 
-    let gameData = [];
+		// Loop over json objects to create game data array
+		for (let i = 0; i < pairNumber; ++i) {
 
-// Loop over json objects to create game data array
-    for (let i = 0; i < pairNumber; ++i) {
-        
-        // Retrieve json object
-        let oneBird = json[i];
+			// Retrieve json object
+			let oneBird = json[i];
 
-        // Add id number in order to match tiles
-        oneBird.id = i;
-       
-        gameData.push(oneBird);
-        gameData.push(oneBird);
-    }
-    
-// Shuffle an array code taken from:
-// https://medium.com/@nitinpatel_20236/how-to-shuffle-correctly-shuffle-an-array-in-javascript-15ea3f84bfb
-    function shuffle(array) {
-        for(let i = array.length - 1; i > 0; i--){
-            let j = Math.floor(Math.random() * i);
-            let temp = array[i];
-            array[i] = array[j];
-            array[j] = temp;
-        }
-    }
-// Shuffle the tiles
-    shuffle(gameData)
+			// Add id number in order to match tiles
+			oneBird.id = i;
 
-// Set up CSS for this number of tiles
-    let numRows = Math.sqrt(maxTiles);
-    $("#game-board").css("grid-template-columns", "repeat(" + numRows + ", 1fr)");
+			gameData.push(oneBird);
+			gameData.push(oneBird);
+		}
 
-    function createTile(gameObject) {
-        let scene = $(
-            `<div class="scene inherit-height">
+		// Shuffle an array code taken from:
+		// https://medium.com/@nitinpatel_20236/how-to-shuffle-correctly-shuffle-an-array-in-javascript-15ea3f84bfb
+		function shuffle(array) {
+			for (let i = array.length - 1; i > 0; i--) {
+				let j = Math.floor(Math.random() * i);
+				let temp = array[i];
+				array[i] = array[j];
+				array[j] = temp;
+			}
+		}
+		// Shuffle the tiles
+		shuffle(gameData);
+
+		// Set up CSS for this number of tiles
+		let numRows = Math.sqrt(maxTiles);
+		$("#game-board").css("grid-template-columns", "repeat(" + numRows + ", 1fr)");
+
+		function createTile(gameObject) {
+			let scene = $(
+				`<div class="scene inherit-height">
                 <div class="tile">
                     <div class="tile-back tile-face"></div>
                     <div class="tile-front tile-face"></div>
                 </div>
             </div>`);
 
-        // Fill in image path
+			// Fill in image path
 
-        // Select second img tag from tile
-        let frontImage = scene.find(".tile-front");
-        // Change css to match image path
-        frontImage.css("background-image", "url(assets/images/" + gameObject.tileImg + ")");
-        
-        // SET id as data tag on tile (for matching later)
-        let tile = scene.find(".tile");
-        tile.attr("tileid", gameObject.id)
+			// Select second img tag from tile
+			let frontImage = scene.find(".tile-front");
+			// Change css to match image path
+			frontImage.css("background-image", "url(assets/images/" + gameObject.tileImg + ")");
 
-        $("#game-board").append(scene);
-    }
+			// SET id as data tag on tile (for matching later)
+			let tile = scene.find(".tile");
+			tile.attr("tileid", gameObject.id);
 
-// Lay out cards randomly
-    for (let i = 0; i < maxTiles; i++) {
+			$("#game-board").append(scene);
+		}
 
-        createTile(gameData[i]);
-    }
-    
-// Flip the cards
-    function onClicked() {
+		// Lay out cards randomly
+		for (let i = 0; i < maxTiles; i++) {
+			createTile(gameData[i]);
+		}
 
-        // Prevent flipping of matched or already flipped cards
-        if ($(this).hasClass('is-flipped') || $(this).hasClass("is-matched"))
-            return;
+		// Flip the cards
+		function onClicked() {
+			// Prevent flipping of matched or already flipped cards
+			if ($(this).hasClass('is-flipped') || $(this).hasClass("is-matched"))
+				return;
 
-        // Only flip two cards
-        const flippedCount = $('.is-flipped').length;
-        if (flippedCount < 2) {
-            $(this).toggleClass("is-flipped");
-        }
+			// Only flip two cards
+			const flippedCount = $('.is-flipped').length;
+			if (flippedCount < 2) {
+				$(this).toggleClass("is-flipped");
+			}
 
-        matchTiles();
-    }
+			matchTiles();
+		}
 
-    function createMatchedModal(index) {
+		function createMatchedModal(index) {
+			let gameObject = json[index];
 
-        let gameObject = json[index];
+			$("#matched-modal .modal-title").html(gameObject.name);
+			$("#matched-modal .modal-subtitle").html(gameObject.latin);
+			$("#matched-modal .modal-pic").attr("src", "assets/images/" + gameObject.tileImg);
+			$("#matched-modal .fact").html(gameObject.text);
 
-        $("#matched-modal .modal-title").html(gameObject.name);
-        $("#matched-modal .modal-subtitle").html(gameObject.latin);
-        $("#matched-modal .modal-pic").attr("src", "assets/images/" + gameObject.tileImg);
-        $("#matched-modal .fact").html(gameObject.text);
+			$("#matched-modal").modal();
 
-        $("#matched-modal").modal();
+			$("#matched-modal").on($.modal.AFTER_CLOSE, function() {
 
-        $("#matched-modal").on($.modal.AFTER_CLOSE, function() {
+				window.setTimeout(checkCompletion, 100);
+			});
+		}
 
-            window.setTimeout(checkCompletion, 100);
-        });
-    }
+		function matchTiles() {
+			// Check for matching pairs
+			let flippedTiles = $(".is-flipped");
+			if (flippedTiles.length == 2) {
 
-    function matchTiles() {
-        // Check for matching pairs
-        let flippedTiles = $(".is-flipped");
-        if (flippedTiles.length == 2) {
+				let tile1 = $(flippedTiles[0]);
+				let tile2 = $(flippedTiles[1]);
 
-            let tile1 = $(flippedTiles[0]);
-            let tile2 = $(flippedTiles[1]);
-            
-            let tile1Id = tile1.attr("tileid");
-            let tile2Id = tile2.attr("tileid");
+				let tile1Id = tile1.attr("tileid");
+				let tile2Id = tile2.attr("tileid");
 
-            // MATCH FOUND
-            if (tile1Id == tile2Id) {
-                
-                //Delay matching until flip animation finished
-                window.setTimeout(function()  {
+				// MATCH FOUND
+				if (tile1Id == tile2Id) {
 
-                    tile1.removeClass("is-flipped").addClass("is-matched");
-                    tile2.removeClass("is-flipped").addClass("is-matched");
+					//Delay matching until flip animation finished
+					window.setTimeout(function() {
 
-                    createMatchedModal(tile1Id);
+						tile1.removeClass("is-flipped").addClass("is-matched");
+						tile2.removeClass("is-flipped").addClass("is-matched");
 
-                }, 1000);
-            }
-            // NO MATCH FOUND
-            else {
+						createMatchedModal(tile1Id);
 
-                window.setTimeout(function()  {
+					}, 1000);
+				}
+				// NO MATCH FOUND
+				else {
 
-                    tile1.removeClass("is-flipped");
-                    tile2.removeClass("is-flipped");
-                }, 1000);
-            }
-        }
-    }
+					window.setTimeout(function() {
 
-    function checkCompletion() {
-        
-        //if all tiles have is-matched class, then show modal
-        if ($(".is-matched").length == maxTiles) {
-            $("#complete-modal").modal({
-                showClose: false,
-                clickClose: false,
-                escapeClose: false
-            });
-        }
-    }
+						tile1.removeClass("is-flipped");
+						tile2.removeClass("is-flipped");
+					}, 1000);
+				}
+			}
+		}
 
-    
-    $(".tile").click(onClicked);
+		function checkCompletion() {
+			//if all tiles have is-matched class, then show modal
+			if ($(".is-matched").length == maxTiles) {
+				$("#complete-modal").modal({
+					showClose: false,
+					clickClose: false,
+					escapeClose: false
+				});
+			}
+		}
 
+		$(".tile").click(onClicked);
 
-    // DEBUG: Used for testing
-    window.testing = {
-        complete: function() {
-            $(".tile").addClass("is-matched");
-            checkCompletion();
-        }
-    };
+		// DEBUG: Used for testing
+		window.testing = {
+			complete: function() {
+				$(".tile").addClass("is-matched");
+				checkCompletion();
+			}
+		};
+
+	});
 
 });
-
-});
-
-
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,14 +1,12 @@
 $(document).ready(function() {
 
-    
-// To highlight the buttons
+    // To highlight the buttons
     $(".button-group button").click(function() {
         $(this).siblings().removeClass("highlight-button");
         $(this).addClass("highlight-button");
     });
 
     $("#play").click(function() {
-
         let category = $("#category .button-group .highlight-button").attr("value");
         sessionStorage.setItem("category", category);
 
